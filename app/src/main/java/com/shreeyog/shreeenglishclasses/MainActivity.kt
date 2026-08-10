2bbc28a05d5163ed1d11c59638f91799b5151d08
Dadu2122 <sharmatatu355@gmail.com>
2026-08-10 14:40:52 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/shreeyog/shreeenglishclasses/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.shreeyog.shreeenglishclasses
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private val siteUrl = "https://dadu2122.github.io/Shree-English-Classes/"
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.databaseEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        if (savedInstanceState == null) {
+            webView.loadUrl(siteUrl)
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
